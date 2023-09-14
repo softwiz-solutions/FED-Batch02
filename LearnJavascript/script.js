f434aaebd02e12aedcf946f064795612486ecd53
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-09-14 16:41:29 +0500

    fed batch02

---
+++ LearnJavascript/script.js
@@ -0,0 +1,50 @@
+// alert("hello")
+
+// keywords
+// whhcih have fixed meanning
+// variable declarations
+// var age,name1;
+// // variable intialization
+// age=12;
+
+// // variabvle declaration and initialization
+// // var age=12;
+// name1="waqas"
+
+
+// varaible naming rules;
+/**
+ * 1. 0-9,A-Z,a-z,_
+ * 2- variable name must start from a-z or _ not with 0-9
+ example: 
+ correct--> age, studentName,_age,age1,age2,age3
+ wrong--> 1age, 2studentName
+ * 3. don't use keyword as a variable name
+*/
+// write a js program to intialize a varaible about your age;
+// var age=20;
+// var age;
+// age = 12;
+
+// var age = 12;//number
+// console.log("the value of age " + age,"fatherName",fathername)// contatination
+// var fathername = "false";//string
+// console.log("the value of fathername", fathername)
+// var present = true//booleean;
+// console.log("the value of present", present)
+
+// DATA TYPES
+
+//  your_first_name="jfsdkfl"// snake case
+//  yourFristName="jfklsdjlfkd"// camle case
+// YourFirstName="jfsldkfj;sdf";
+// console.log("the type of age is ",typeof(age));
+// var age = 12;
+// console.log("age",age);
+// age = 123;
+// console.log("age ", age);
+var age = 12;
+var number = 145;
+age = number;
+console.log("age", age);
+console.log("number",number);
