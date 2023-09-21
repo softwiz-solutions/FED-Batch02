e43b4cfe9923c31b297ef44a901732c6f1f1e408
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-09-21 17:28:04 +0500

    if statement

---
+++ LearnJavascript/condition.js
@@ -0,0 +1,80 @@
+// condtion
+// 1.if
+// 2. if else
+//3. if else if
+// 4. nested
+
+// var input = prompt("enter value");
+// var value = parseInt(input)
+
+// var age = parseInt(prompt("enter your value"));
+
+// syntax
+// if (condition) {
+// // body
+// }
+
+// if (input > 10) {
+//     console.log("input is greater then 10");
+// }
+
+// if (age >= 18 && age <= 25) {
+//     console.log("age --->", age);
+// }
+
+
+// if -else
+// if (age > 18 && age < 25) { // true
+//     console.log("yuyou age is b/w 18 and 25")
+// }
+// else { // false
+//     console.log("you age is not b/w 18 and 25");
+// }
+
+
+// var result = (age > 18 && age < 25) ? "your age is b / w 18 and 25" : "your age is ot b/w 18 and 25";
+
+// console.log("result", result);
+
+// multiple condition
+// if else if
+
+// if (condition){
+
+// }
+// else if(condition){
+
+// }
+// .....
+// else{
+
+// }
+
+
+// var weekDay = parseInt(prompt("ente week day"))
+
+
+// if (weekDay == 1) {
+//     console.log("Monday");
+// }
+// else if (weekDay == 2) {
+//     console.log("tue");
+// }
+// else if (weekDay == 3) {
+//     console.log("wed");
+// }
+// else if (weekDay == 4) {
+//     console.log("thur");
+// }
+// else if (weekDay == 5) {
+//     console.log("fri");
+// }
+// else if (weekDay == 6) {
+//     console.log("sat");
+// }
+// else if (weekDay == 7) {
+//     console.log("sun");
+// }
+// else{
+//     console.log("please enter valid week day");
+// }
