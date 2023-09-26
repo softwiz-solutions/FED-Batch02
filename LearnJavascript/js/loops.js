6942201c975cd2f3af76ca13a84c39cd26ff4f46
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-09-26 14:09:51 +0500

    arrays

--- LearnJavascript/js/loops.js
+++ LearnJavascript/js/loops.js
@@ -3,8 +3,58 @@
 // 2. for loop
 // 3 . do while loop
 
+/*
+1. initalize
+2. condition check
+3. body execute
+4. increment/drecment
+*/
 // var counter = 0;
 // while (counter < 10) {
-//     console.log("hello world", counter);
+//     console.log("hello world in while loop", counter);
 //     counter++
 // }
+
+// 2. for loop
+// for(initiazliztion;conditon;increcement/decremenet){
+// body
+// }
+
+// for (var i = 0; i < 10; i++) {
+//     console.log("hello world");
+// }
+
+// do while
+// 1. initalize
+// 2. body execute
+// 4. increment/drecment
+// 3. condition check
+// var counter2 = 0;
+// do {
+//     console.log("hello wold in do loop",counter2);
+//     counter2++;
+// } while (counter2 < 10)
+
+
+// arrays
+// var studentName1="ali"
+// var studentName2="ali"
+// var studentName3="ali"
+// var studentName4="ali"
+// var studentName5="ali"
+// var studentName6="ali"
+// var studentName7="ali"
+
+
+// arrays
+let studentName = ["ali","usama","ahmad"]
+// console.log("first student",studentName[0]);
+// console.log("second student",studentName[1]);
+// console.log("third student",studentName[2]);
+// studentName[0]="ali"
+// studentName[1]="usama"
+// console.log("studename",studentName);
+
+for(var i=0;i<3;i++){
+    console.log("student nhame",studentName[i]);
+}
