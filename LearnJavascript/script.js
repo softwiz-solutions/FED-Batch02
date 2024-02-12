25593e86ea0fd75d35e61fc6b903f74941014e98
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-02-12 19:41:33 +0500

    js lecture

---
+++ LearnJavascript/script.js
@@ -0,0 +1,22 @@
+// programming language:
+//  the language used for communicating with computer
+// 1. program
+// set of instructions in programming language give to the computer to do some tasks
+// Binary Language (0,1)
+// a>10102
+// b>10103
+// Compiler
+// pluse two numbers > compiler> 010010 10001 101001
+// syntax rule same like grammer rule
+
+//reserve words/ keywords (in english has, have, is , are ,am , many , there, here, wherer)
+// (in pl for, name, if, swtic, case,var,let,const)
+
+//  benefits of lerning javascript
+// web development --> frontend,backend (nodejs)
+//  app development --> react native
+//  destop application--> electron js
+
+
+
+
