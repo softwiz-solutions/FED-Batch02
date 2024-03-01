0451503f2e60c25f2517b2a799f469c12d366ed2
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-03-01 19:52:41 +0500

    js loops

--- LearnJavascript/conditions.js
+++ LearnJavascript/conditions.js
@@ -56,29 +56,53 @@
 
 //  2+2=4, 3+1=4, 5-1=4, 4+0=4;
 
-var dayNumber = parseInt(prompt("Please enter a day number"));
-switch (dayNumber) {
-  case 1: // case dayNumber==1
-    console.log("Monday");
-    break;
-  case 2:
-    console.log("Tues");
-    break;
-  case 3:
-    console.log("Wed");
-    break;
-  case 4:
-    console.log("Thursday");
-    break;
-  case 5:
-    console.log("Fri");
-    break;
-  case 6:
-    console.log("Sat");
-    break;
-  case 7:
-    console.log("Sun");
-    break;
-  default:
-    console.log("please enter number b/w 1 to 7");
-}
+// var dayNumber = parseInt(prompt("Please enter a day number"));
+// switch (dayNumber) {
+//   case 1: // case dayNumber==1
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tues");
+//     break;
+//   case 3:
+//     console.log("Wed");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Fri");
+//     break;
+//   case 6:
+//     console.log("Sat");
+//     break;
+//   case 7:
+//     console.log("Sun");
+//     break;
+//   default:
+//     console.log("please enter number b/w 1 to 7");
+// }
+
+// if (condition) {
+//   if (condition) {
+//   } else if (condtion) {
+//   }
+// } else {
+// }
+
+// write a program to check the user age, if the age of user is greater than pr equal to 18 , then check that if the age is b/w 18 to 25 , show you are young otherwise show you are older, and if the age is less then 18 , check that if age is between 10 to 17 show that you are too younger otherwise show that you are chold,
+
+// var age = parseInt(prompt("Please enter your age"));
+// if (age >= 18) {
+//   if (age >= 18 && age <= 25) {
+//     console.log("You are young");
+//   } else {
+//     console.log("you are older");
+//   }
+// } else {
+//   if (age > 10 && age < 18) {
+//     console.log("you are too younger");
+//   } else {
+//     console.log("you are child");
+//   }
+// }
--- LearnJavascript/loop.js
+++ LearnJavascript/loop.js
@@ -5,6 +5,12 @@
 // 2. do while loop
 // 3. for loop
 
+// *********while loop********
+// 1. intialization
+// 2. condtion
+// 3. body execution
+// 4. increment/decrement
+
 // var counter = 10; // initialization
 // while (counter >= 0) {
 //   // condtion
@@ -13,3 +19,41 @@
 //   counter--; // increment
 // }
 // console.log("your loop hase been stoped");
+
+// *********do while loop********
+// 1. intialization
+// 2. body execution
+// 3. increment/decrement
+// 4. condtion
+
+// var i = 0;
+// do {
+//   console.log("hello");
+//   i++;
+// } while (i < 10);
+// var ans;
+// do {
+//   console.log("hello");
+//   //   i++;
+//   ans = prompt("Do you want to proceed? y/n");
+// } while (ans == "y");
+
+// for loop
+// *********for loop********
+// 1. intialization
+// 2. condtion
+// 3. body execution
+// 4. increment/decrement
+
+// for (var i = 0; i < 10; i++) {
+//   console.log("hello");
+// }
+
+//  in while loop we don't know the number of iterations
+// in for loop we know the number of iteration
+//  while loop
+// var ans = "y";
+// while (ans == "y") {
+//   console.log("hello");
+//   ans = prompt("Do you want to proceed? y/n");
+// }
