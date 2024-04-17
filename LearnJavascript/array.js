9ca7daefc9a0943704bb06c528833760ceec2a28
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-04-17 19:20:49 +0500

    before ramzan

--- LearnJavascript/array.js
+++ LearnJavascript/array.js
@@ -81,7 +81,7 @@
 
 // console.log("studentsName", studentsName);
 
-var studentsName = ["ali", "ahmad", "usama", "muawiya"];
+// var studentsName = ["ali", "ahmad", "usama", "muawiya"];
 
 // TODO: for add  element anywerhere in array
 // var deltedElement = studentsName.splice(0, 1,"muawiya");
@@ -93,7 +93,89 @@ var studentsName = ["ali", "ahmad", "usama", "muawiya"];
 // studentsName.slice(0, 2);
 // console.log("students", studentsName);
 
-
 // var getElements=studentsName.slice(0,4)
 // console.log("getelement",getElements)
-// console.log("studentsName",studentsName)
+// console.log("studentsName",studentsName)[1, 2, 3].push(4); // [1, 2, 3, 4]
+// [1, 2, 3].pop(); // [1, 2]
+// [1, 2, 3].shift(); // [2, 3]
+// [1, 2, 3].unshift(0); // [0, 1, 2, 3]
+// ['a', 'b'].concat('c'); // ['a', 'b', 'c']
+// ['a', 'b', 'c'].join('-'); // a-b-c
+// ['a', 'b', 'c'].slice(1); // ['b', 'c']
+// ['a', 'b', 'c'].indexOf('b'); // 1
+// ['a', 'b', 'c'].includes('c'); // true
+// [3, 5, 6, 8].find((n) => n % 2 === 0); // 6
+// [2, 4, 3, 5].findIndex((n) => n % 2 !== 0); // 2
+// [3, 4, 8, 6].map((n) => n * 2); // [6, 8, 16, 12]
+// [1, 4, 7, 8].filter((n) => n % 2 === 0); // [4, 8]
+// [2, 4, 3, 7].reduce((acc, cur) => acc + cur); // 16
+// [2, 3, 4, 5].every((x) => x < 6); // true
+// [3, 5, 6, 8].some((n) => n > 6); // true
+// [1, 2, 3, 4].reverse(); // [4, 3, 2, 1]
+// [3, 5, 7, 8].at(-2); // 7
+
+// concat
+// var studentName = ["ali", "ahmad", "usama"];
+// var fruitsName = ["apple", "orange", "mango"];
+// var teacherName = ["bilal", "ahmad", "naveed"];
+// ["ali", "ahmad", "usama", "apple", "orange", "mango"];
+
+// var newArray = studentName.concat(fruitsName, teacherName);
+// var newArray = [...studentName, ...fruitsName, ...teacherName];
+// console.log("newArray", newArray);
+
+// flat
+// var newArray = [studentName, fruitsName, teacherName];
+// console.log("newArray", newArray.flat());
+// console.log("new array", newArray[0][1]);
+
+// join
+
+// var newArray = [///studentName, fruitsName, teacherName];
+// var fruitsName = ["apple", "orange", "mango"];
+// var joinedArray = fruitsName.join("");
+// console.log("new array joint", joinedArray);
+// console.log("new array", fruitsName);
+
+// map
+// var number = [2, 5, 6];
+// for (var i = 0; i < number.length; i++) {
+//   number[i] = number[i] * 3;
+// }
+// console.log("values", number);
+// var numbers = [2, 5, 6];
+// var newArray = numbers.map((item, index) => {
+//   return item * 3;
+// });
+
+// console.log("number", numbers);
+// console.log("newArray", newArray);
+
+// filter
+// var numbers = [2, 5, 6];
+// var filtedArray = numbers.filter((item) => {
+//   return item > 3;
+// });
+// console.log("filtered array", filtedArray);
+
+// var studentNames = ["ali", "ahmad", "mujtb"];
+// var filtedArray = studentNames.filter((item) => {
+//   return item.length == 2;
+// });
+// console.log("filted", filtedArray);
+
+// changing case
+
+// var studentName = "Ahmad";
+// console.log("uppwercase", studentName.toLowerCase());
+
+// var studentNames = ["ali", "ahmad", "mujtb", "abdul hanan"];
+// var lowerCaseStudentsName = studentNames.map((item, index) => {
+//   //   return item.toUpperCase();
+//   if (index % 2 !== 0) {
+//     return item.toLowerCase();
+//   }
+//   return item.toUpperCase();
+// });
+// console.log("studentNames", studentNames);
+// console.log("lowerCaseStudentsName", lowerCaseStudentsName);
