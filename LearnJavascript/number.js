8ecd2d88c6a9c1230a3e52a4ae1ca980eef2dbd6
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-04-18 20:04:47 +0500

    math methods

---
+++ LearnJavascript/number.js
@@ -0,0 +1,23 @@
+// chapter no 26
+// var input = parseInt(prompt("Enter your value"));
+// var number = Math.random() * input;
+// console.log("random number", number);
+
+// var myNumber = Math.ceil(34.1);
+// console.log("my number", myNumber);
+// var myNumber = Math.floor(34.9090909);
+// console.log("my number", myNumber);
+// var myNumber = Math.round(34.2090909);
+// console.log("my number", myNumber);
+// var number = Math.floor(Math.random() * 3 + 1);
+// // var roundOffNumber = Math.ceil(number);
+// // console.log("round of number", roundOffNumber);
+// console.log("number", number);
+
+// write a js program, to make a toss game, if the value is 0 then show tail wins if value is 1 show head wins
+// var number = "23";
+// console.log("number", Number(number) + 1, "simple value", number + 1);
+// var number = 234;
+// console.log("string", number.toString());
+var number=23.34343;
+console.log("number",number.toFixed(1))
