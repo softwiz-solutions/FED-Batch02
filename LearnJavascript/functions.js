33a9e9336fc48b6c2f54a6a8d4c9ce71cf410fb7
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-04-23 19:58:30 +0500

    changes

---
+++ LearnJavascript/functions.js
@@ -0,0 +1,38 @@
+// console.log("hello");
+
+// var now = new Date();
+// var theHr = now.getHours();
+// var theMin = now.getMinutes();
+// console.log("time: " + theHr + ":" + theMin);
+
+// function getMyFullTime() {
+//   var now = new Date();
+//   var theHr = now.getHours();
+//   var theMin = now.getMinutes();
+//   console.log("time: " + theHr + ":" + theMin);
+// }
+// getMyFullTime()
+
+// parameters
+// function sayHello(name, age) {
+//   console.log("hello my name is " + name, "my age is ", age);
+// }
+
+// // sayHello();
+// argument passing
+// sayHello(23, "abdul hanna");
+// sayHello("zainab");
+// sayHello("muwiya");
+// var names = ["ali", "ahmad", "haider"];
+// names.forEach((data) => {
+//   // console.log("name",data)
+//   sayHello(data);
+// });
+
+// function
+
+
+// var result =a+b;
+// if(opera=="+"){
+//     sum(a,b)
+// }
