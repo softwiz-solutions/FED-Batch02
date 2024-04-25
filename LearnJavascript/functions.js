b16aee9fdad72fb0206f82aefcf4f94be9d67cf3
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-04-25 19:27:41 +0500

    js functions

--- LearnJavascript/functions.js
+++ LearnJavascript/functions.js
@@ -31,8 +31,64 @@
 
 // function
 
-
 // var result =a+b;
 // if(opera=="+"){
 //     sum(a,b)
 // }
+
+// function sayHello(name, age) {
+//   console.log("hello world!", name, age);
+// }
+// var myName = "abdul hanan";
+// sayHello(myName, 23);
+
+// function add(num1, num2) {
+//   var result = num1 + num2;
+//   console.log("rsul", result);
+//   return result;
+//   var value;
+//   if (result > 10) {
+//     value = "rseul is greatere than 10";
+//   } else {
+//     value = "rseul is less than 10";
+//   }
+//   return value;
+// }
+// add(23, 3);
+// var ans = 3 + add(2, 3);
+// if (ans > 3) {
+//   console.log("abd");
+// }
+
+// console.log("object", ans);
+
+//  var local and global scope
+// var result = 234;
+// function showResult() {
+//   var result = 4;
+//   result = 34;
+//   console.log("function", result);
+// }
+// showResult();
+// console.log("main console", result);
+
+//  let local and global scope
+// const result = 234;
+// const result=34;
+// result=34;
+// function showResult() {
+//   let result = 4;
+//   //   result = 34;
+//   //   result = 34;
+//   console.log("function", result);
+// }
+// showResult();
+// console.log("main console", result);
+// result = 34;
+// console.log("result", result);
+// let result ;
+
+// 1. write a program, to say hello using function
+// 2.write a program, user will enter his name and then answer will show "hello username" using function;
+// like hello ali
+// 3. write a program, function take two values and return the sum of both of these values;
