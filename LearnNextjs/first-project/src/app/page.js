2a81e788ecaa957cad62c48d23369dfac909143d
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-06-11 20:22:26 +0500

    component

--- LearnNextjs/first-project/src/app/page.js
+++ LearnNextjs/first-project/src/app/page.js
@@ -2,109 +2,58 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const myFavourtieBooks = ["comp", "bio", "eng", "math", "urdu"];
   return (
-    <main className={styles.main}>
-      <Button />
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>Abdul hanan</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <div className="main">
+        {/* <h1>hello world</h1> */}
+        <h1>my favotir books</h1>
+        <ul>
+          {myFavourtieBooks.map((item, index) => {
+            return <List title={item} key={index} />;
+          })}
+          {/* <List title="comp" />
+          <List title="Bio" />
+          <List title="Engli" /> */}
+        </ul>
+        <Heading />
+        <MyButton />
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-      <Button />
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-        <Button />
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-        <Button />
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="main">
+        {/* <h1>hello world</h1> */}
+        <h1>my favotir books</h1>
+        <ul>
+          <List title="comp" />
+          <List title="Bio" />
+          <List title="Engli" />
+        </ul>
+        <Heading title="my custome heading" para="this is my custome para" />
+        <MyButton />
       </div>
     </main>
   );
 }
 
-function Button() {
-  const value = 234;
+const MyButton = () => {
+  return <button>hello</button>;
+};
+
+const Heading = (props) => {
+  // const Heading = ({ title, para }) => {
+  console.log("props", props);
+  const { para, title } = props;
   return (
-    <div style={{ border: "3px solid yellow" }}>
-      <h1>Hello this is card {value}</h1>
-      <p>loremjfsd fljds kk fjsldk f </p>
-      <button>Click me 2</button>
+    <div>
+      {/* <h1>{props.title ? props.title : "My default heading"}</h1>
+      <p>{props.para ? props.para : "my default para"}</p> */}
+      {/* <h1>{title ? title : "My default heading"}</h1>
+      <p>{para ? para : "my default para"}</p> */}
+      <h1>hello {title} world</h1>
     </div>
   );
-}
+};
+
+const List = (props) => {
+  const { title } = props;
+  return <li>{title}</li>;
+};
