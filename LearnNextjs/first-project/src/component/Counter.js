cd4082ccd31a08123a76ccb1a8a899948338d3e6
abdul-hanan79 <abdulhanan3279@gmail.com>
2024-06-26 20:17:01 +0500

    couter app

--- LearnNextjs/first-project/src/component/Counter.js
+++ LearnNextjs/first-project/src/component/Counter.js
@@ -4,21 +4,33 @@ import MyButton from "./MyButton";
 
 const Counter = () => {
   //   let value = 2;
-  const [value, setValue] = useState(1);
-  const [showHeading, setShowHeading] = useState(false);
-  const handleButton = () => {
-    setShowHeading(!showHeading);
-    // console.log("clicked");
-    // // value++;
-    // let counter = value;
-    // setValue(++counter);
-    // console.log("🚀 ~ handleButton ~ value:", value);
+  const [value, setValue] = useState(0);
+  // const [counter,setCounter]=useState(0)
+  // const [showHeading, setShowHeading] = useState(false);
+  // const handleButton = () => {
+  //   setShowHeading(!showHeading);
+  //   // console.log("clicked");
+  //   // // value++;
+  //   // let counter = value;
+  //   // setValue(++counter);
+  //   // console.log("🚀 ~ handleButton ~ value:", value);
+  // };
+  const increment = () => {
+    setValue(value + 1);
+  };
+  const decrement = () => {
+    setValue(value - 1);
+  };
+  const reset = () => {
+    setValue(0);
   };
   return (
     <div>
-      <p className="description">value: {value}</p>
-      {showHeading && <h1>heading</h1>}
-      <MyButton title="count" handleClick={handleButton} />
+      <p className="text-3xl">value: {value}</p>
+      {/* {showHeading && <h1>heading</h1>} */}
+      <MyButton title="Increment" handleClick={increment} />
+      <MyButton title="Decrement" handleClick={decrement} />
+      <MyButton title="Reset" handleClick={reset} />
     </div>
   );
 };
